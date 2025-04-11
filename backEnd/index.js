e6689757afe235153c9d8011098052d8a90f5da5
caiomps <caiomportelasousann@gmail.com>
2025-04-11 10:29:39 -0300

    feita a conexao do banco de dados sql utilizando o sequelize

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const conn = require("./db/conn");
 
 const port = 3000;
+
+//conectando o banco de dados sql
+conn();
+
+//conectando o front
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("Servidor rodadando na porta " + port);
